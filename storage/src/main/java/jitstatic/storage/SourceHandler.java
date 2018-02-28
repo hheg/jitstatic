bedf2adc2faef5881cf35d0a0de8def2eebfafce
hheg <henrikhegardtgithub@gmail.com>
2018-02-28 21:14:51 +0100

    Changed JSON output

--- storage/src/main/java/jitstatic/storage/SourceHandler.java
+++ storage/src/main/java/jitstatic/storage/SourceHandler.java
@@ -31,14 +31,15 @@ import com.fasterxml.jackson.core.JsonParser.Feature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 import jitstatic.JitStaticConstants;
 import jitstatic.StorageData;
 
 class SourceHandler {
 	private static final ObjectMapper MAPPER = new ObjectMapper().enable(Feature.ALLOW_COMMENTS);
-	private static final ObjectWriter WRITER = MAPPER.writerWithDefaultPrettyPrinter();
-	
+	private static final ObjectWriter WRITER = MAPPER.writerWithDefaultPrettyPrinter().with(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS);
+
 	public byte[] readStorageData(final InputStream is, final String type) throws JsonParseException, JsonMappingException, IOException {
 		Objects.requireNonNull(type);
 		return readByteArray(is);
@@ -51,9 +52,9 @@ class SourceHandler {
 		}
 	}
 
-	public byte[] readByteArray(final InputStream is) throws IOException {
+	private byte[] readByteArray(final InputStream is) throws IOException {
 		Objects.requireNonNull(is);
-		ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+		final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
 		int nRead;
 		byte[] data = new byte[4096];
 		while ((nRead = is.read(data, 0, data.length)) != -1) {
@@ -62,6 +63,7 @@ class SourceHandler {
 		buffer.flush();
 		return buffer.toByteArray();
 	}
+
 	// TODO Don't make this pass
 	public byte[] formatData(final byte[] data, final String contentType) throws IOException {
 		Objects.requireNonNull(data);
