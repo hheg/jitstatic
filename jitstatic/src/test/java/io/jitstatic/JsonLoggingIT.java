58130a979f7dbb7532e1edd69f7250a5888fe51f
hheg <henrikhegardtgithub@gmail.com>
2019-10-03 00:55:52 +0200

    Added JSON logging capabilites

---
+++ jitstatic/src/test/java/io/jitstatic/JsonLoggingIT.java
@@ -0,0 +1,187 @@
+package io.jitstatic;
+
+/*-
+ * #%L
+ * jitstatic
+ * %%
+ * Copyright (C) 2017 - 2019 H.Hegardt
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.UncheckedIOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
+import java.util.function.Supplier;
+import java.util.stream.StreamSupport;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.transport.PushResult;
+import org.eclipse.jgit.transport.RemoteRefUpdate.Status;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.dropwizard.testing.ConfigOverride;
+import io.dropwizard.testing.ResourceHelpers;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import io.jitstatic.client.APIException;
+import io.jitstatic.client.JitStaticClient;
+import io.jitstatic.client.JitStaticClientBuilder;
+import io.jitstatic.client.TriFunction;
+import io.jitstatic.hosted.HostedFactory;
+import io.jitstatic.test.TemporaryFolder;
+import io.jitstatic.test.TemporaryFolderExtension;
+
+@ExtendWith({ TemporaryFolderExtension.class, DropwizardExtensionsSupport.class })
+public class JsonLoggingIT {
+    private static final String METADATA = ".metadata";
+    private static final String REFS_HEADS_MASTER = "refs/heads/master";
+    private static final String ACCEPT_STORAGE = "accept/storage";
+    private static final String USER = "suser";
+    private static final String PASSWORD = "ssecret";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    private DropwizardAppExtension<JitstaticConfiguration> DW = new DropwizardAppExtension<>(JitstaticApplication.class, ResourceHelpers
+            .resourceFilePath("simpleserver_json.yaml"), ConfigOverride.config("hosted.basePath", getFolder()));
+    private TemporaryFolder tmpfolder;
+    private String adress;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        adress = String.format("http://localhost:%d/application", DW.getLocalPort());
+        HostedFactory hostedFactory = DW.getConfiguration().getHostedFactory();
+        String user = hostedFactory.getUserName();
+        String pass = hostedFactory.getSecret();
+        String servletName = hostedFactory.getServletName();
+        String endpoint = hostedFactory.getHostedEndpoint();
+
+        setupRepo(user, pass, servletName, endpoint);
+    }
+
+    private void setupRepo(String user, String pass, String servletName, String endpoint) throws Exception {
+        File workingDirectory = getFolderFile();
+        UsernamePasswordCredentialsProvider provider = new UsernamePasswordCredentialsProvider(user, pass);
+        try (Git git = Git.cloneRepository().setDirectory(workingDirectory).setURI(adress + "/" + servletName + "/" + endpoint).setCredentialsProvider(provider)
+                .call()) {
+            writeFile(workingDirectory.toPath(), ACCEPT_STORAGE);
+            writeFile(workingDirectory.toPath(), ACCEPT_STORAGE + METADATA);
+
+            final Path filePath = workingDirectory.toPath().resolve("accept/.metadata");
+            Files.createDirectories(Objects.requireNonNull(filePath.getParent()));
+            try (InputStream is = getClass().getResourceAsStream("/accept/metadata")) {
+                Files.copy(is, filePath, StandardCopyOption.REPLACE_EXISTING);
+            }
+
+            writeFile(workingDirectory.toPath(), "accept/genkey");
+            git.add().addFilepattern(".").call();
+            git.commit().setMessage("Initial commit").call();
+            Iterable<PushResult> call = git.push().setCredentialsProvider(provider).call();
+            assertTrue(StreamSupport.stream(call.spliterator(), false).allMatch(p -> p.getRemoteUpdate(REFS_HEADS_MASTER).getStatus() == Status.OK));
+        }
+    }
+
+    @Test
+    public void testJSONLogging() throws URISyntaxException, APIException, IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(bos);
+        PrintStream oldOutput = System.out;
+        System.setOut(ps);
+        try (JitStaticClient client = buildClient().setUser(USER).setPassword(PASSWORD).build();) {
+            Entity<JsonNode> key = client.getKey(ACCEPT_STORAGE, null, tf);
+            assertEquals(getData(), key.data.toString());
+            assertNotNull(key.getTag());
+            assertTrue(bos.toByteArray().length > 0);
+            JsonNode readTree = MAPPER.readTree(bos.toByteArray());
+            assertEquals("INFO", readTree.findValue("level").asText());
+        } finally {
+            System.setOut(oldOutput);
+        }
+    }
+
+    private Supplier<String> getFolder() {
+        return () -> {
+            try {
+                return getFolderFile().getAbsolutePath();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+
+    private File getFolderFile() throws IOException { return tmpfolder.createTemporaryDirectory(); }
+
+    private JitStaticClientBuilder buildClient() {
+        return JitStaticClient.create().setHost("localhost").setPort(DW.getLocalPort()).setAppContext("/application/");
+    }
+
+    private void writeFile(Path workBase, String file) throws IOException {
+        final Path filePath = workBase.resolve(file);
+        Files.createDirectories(Objects.requireNonNull(filePath.getParent()));
+        try (InputStream is = getClass().getResourceAsStream("/" + file)) {
+            Files.copy(is, filePath, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    static class Entity<T> {
+
+        final T data;
+        private final String tag;
+        private final String contentType;
+
+        public Entity(String tag, String contentType, T data) {
+            this.tag = tag;
+            this.contentType = contentType;
+            this.data = data;
+        }
+
+        public String getTag() { return tag; }
+
+        public String getContentType() { return contentType; }
+    }
+
+    private TriFunction<InputStream, String, String, Entity<JsonNode>> tf = (is, v, t) -> {
+        if (is != null) {
+            try {
+                return new Entity<>(v, t, MAPPER.readValue(is, JsonNode.class));
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+        return new Entity<>(v, t, null);
+    };
+
+    private static String getData() { return getData(0); }
+
+    private static String getData(int c) {
+        return "{\"key" + c
+                + "\":{\"data\":\"value1\",\"users\":[{\"captain\":\"america\",\"black\":\"widow\"}]},\"mkey3\":{\"data\":\"value3\",\"users\":[{\"tony\":\"stark\",\"spider\":\"man\"}]}}";
+    }
+}
